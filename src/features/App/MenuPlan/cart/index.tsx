c269d102e785ee8d3b1a34755a338ac76c586b00
ugdesmond <ugdesmond@gmail.com>
2021-08-21 12:26:30 +0100

    duplicate grouping fixed

--- src/features/App/MenuPlan/cart/index.tsx
+++ src/features/App/MenuPlan/cart/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -19,13 +19,13 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import AccordionListItem from '../../../../components/AccordionList/index';
-import {RadioButton} from '../../../../components/RadioButton/index';
+import { RadioButton } from '../../../../components/RadioButton/index';
 import ModalMessage from '../../../../components/CartMessagesModal';
-import {List} from './innerListItem';
-import {cartData} from './cartData';
-import {styles} from './styles';
-import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
-import {ListItems} from './listItem';
+import { List } from './innerListItem';
+import { cartData } from './cartData';
+import { styles } from './styles';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { ListItems } from './listItem';
 import SimpleHeader from '../../../../components/HeaderBar/simpleHeader';
 import PriceTag from '../../../../components/PriceTag/index';
 import {
@@ -33,15 +33,15 @@ import {
   useNavigation,
   useRoute,
 } from '@react-navigation/native';
-import {EmptyList} from '../../../../components';
+import { EmptyList } from '../../../../components';
 import Modal from 'react-native-modal';
-import {colors} from '../../../../colors';
-import {useDispatch, useSelector} from 'react-redux';
-import {basketStates} from '../../../../reducers/basket';
-import {AppDispatch, RootState} from '../../../../store';
-import {getMenuitemCart, getOrderById} from '../../../../FetchData';
+import { colors } from '../../../../colors';
+import { useDispatch, useSelector } from 'react-redux';
+import { basketStates } from '../../../../reducers/basket';
+import { AppDispatch, RootState } from '../../../../store';
+import { getMenuitemCart, getOrderById } from '../../../../FetchData';
 
-const {width: windowWidth} = Dimensions.get('window');
+const { width: windowWidth } = Dimensions.get('window');
 
 interface ListItemDataProps {
   hour: string;
@@ -88,7 +88,7 @@ export const Cart = () => {
     toggleVisible();
   };
 
-  const groupBasketItem = (item: any) => {
+  const groupBasketItem = () => {
     let basketData: any = [];
 
     basketItem?.forEach((item: any) => {
@@ -100,7 +100,7 @@ export const Cart = () => {
     });
     setgrouped(basketData);
     console.log('====baket items======= ', JSON.stringify(basketData));
-    return item;
+    return basketData;
   };
 
   const getMenuplanKart = async () => {
@@ -151,14 +151,14 @@ export const Cart = () => {
   let ttl = basketItem;
 
   const [isLiked, setIsLiked] = useState([
-    {id: 1, value: true, name: 'QuickTeller', selected: false},
-    {id: 2, value: false, name: 'Pay from My Wallet', selected: false},
+    { id: 1, value: true, name: 'QuickTeller', selected: false },
+    { id: 2, value: false, name: 'Pay from My Wallet', selected: false },
   ]);
   const onRadioBtnClick = (item: any) => {
     let updatedState = isLiked.map((isLikedItem) =>
       isLikedItem.id === item.id
-        ? {...isLikedItem, selected: true}
-        : {...isLikedItem, selected: false},
+        ? { ...isLikedItem, selected: true }
+        : { ...isLikedItem, selected: false },
     );
     setIsLiked(updatedState);
   };
@@ -217,31 +217,35 @@ export const Cart = () => {
     // if the basket item date doesnt exist before
     basketItems.push({
       deliveryDate: itemData.deliveryDate,
-      data: [{planType: itemData.MenuPlan.MenuplanDetail.plantype, itemData}],
+      data: [{ planType: itemData.MenuPlan.MenuplanDetail.plantype, itemData }],
     });
   };
 
   const groupByPlanTypeDate = (basketItems: any) => {
     let planTypeData: any = [];
+    let planTypeArray: any = [];
     for (const item of basketItems) {
       if (planTypeData.length == 0) {
         planTypeData.push({
           planType: item.planType,
-          data: [{itemData: item.itemData}],
+          data: [{ itemData: item.itemData }],
         });
+        planTypeArray.push(item.planType);
       } else {
         for (const planData of planTypeData) {
-          console.log('======hello world=====', planData);
           if (planData.planType == item.planType) {
             if (!checkIfPlanExist(item, planData.data)) {
-              planData.data.push({itemData: item.itemData});
+              planData.data.push({ itemData: item.itemData });
             }
             break;
-          } else {
+          }
+          //Ensure that unique plantype exist 
+          if (!planTypeArray.includes(item.planType)) {
             planTypeData.push({
               planType: item.planType,
-              data: [{itemData: item.itemData}],
+              data: [{ itemData: item.itemData }],
             });
+            planTypeArray.push(item.planType);
           }
         }
       }
@@ -258,14 +262,14 @@ export const Cart = () => {
     return false;
   };
 
-  const ListItem = ({hour, list}: ListItemDataProps) => {
+  const ListItem = ({ hour, list }: ListItemDataProps) => {
     return (
       <View style={styles.listView}>
         <Text style={styles.hour}>{hour}</Text>
         <FlatList
           data={list}
           style={styles.listStyle}
-          renderItem={({item}) => {
+          renderItem={({ item }) => {
             return (
               <List
                 styles={styles}
@@ -290,13 +294,13 @@ export const Cart = () => {
       <KeyboardAvoidingView
         keyboardVerticalOffset={-305}
         behavior={Platform.OS === 'ios' ? 'padding' : 'padding'}
-        style={{flex: 1}}>
-        <SimpleHeader style={{paddingLeft: 10}} />
+        style={{ flex: 1 }}>
+        <SimpleHeader style={{ paddingLeft: 10 }} />
         <View style={styles.root}>
           <FlatList
             data={grouped}
             style={styles.listStyle}
-            renderItem={({item}) => {
+            renderItem={({ item }) => {
               return basketItem.length == 0 ? (
                 <EmptyList
                   image={require('../../../../assets/Images/emptyCart.png')}
@@ -305,14 +309,14 @@ export const Cart = () => {
                   onPress={() => navigation.goBack()}
                 />
               ) : (
-                // <ListItems
-                //   hour={eachTime}
-                //   date={eachDate}
-                //   list={route?.params?.cartItems}
-                //   styles={styles}
-                // />
-                <View>
-                  {/* <List
+                  // <ListItems
+                  //   hour={eachTime}
+                  //   date={eachDate}
+                  //   list={route?.params?.cartItems}
+                  //   styles={styles}
+                  // />
+                  <View>
+                    {/* <List
                       plantime={item?.MenuPlan?.MenuplanDetail?.deliveryTime}
                       date={item?.MenuPlan?.MenuplanDetail?.plandate}
                       // planDetails={Object.values(item)}
@@ -327,11 +331,11 @@ export const Cart = () => {
                       time={item?.MenuPlan?.MenuplanDetail?.plantype}
                       basketId={item?.id}
                     /> */}
-                  <List
-                    plantime={item?.MenuPlan?.MenuplanDetail?.deliveryTime}
-                    date={item?.deliveryDate}
-                    planDetails={item?.data}
-                    styles={styles}
+                    <List
+                      plantime={item?.MenuPlan?.MenuplanDetail?.deliveryTime}
+                      date={item?.deliveryDate}
+                      planDetails={item?.data}
+                      styles={styles}
                     // imageUrl={
                     //   item?.MenuPlan?.MenuplanDetail?.MenuItem?.imageUrl
                     // }
@@ -341,9 +345,9 @@ export const Cart = () => {
                     // count={item?.quantity}
                     // time={item?.MenuPlan?.MenuplanDetail?.plantype}
                     // basketId={item?.id}
-                  />
-                </View>
-              );
+                    />
+                  </View>
+                );
             }}
             // pagingEnabled
             // nestedScrollEnabled
@@ -388,7 +392,7 @@ export const Cart = () => {
                     marginTop: 20,
                   }}>
                   <Text
-                    style={{marginLeft: 15, fontWeight: 'bold', fontSize: 17}}>
+                    style={{ marginLeft: 15, fontWeight: 'bold', fontSize: 17 }}>
                     Create a name for your new plan
                   </Text>
                   <TextInput
@@ -466,8 +470,8 @@ export const Cart = () => {
                       flexDirection: 'row',
                       justifyContent: 'space-between',
                     }}>
-                    <Text style={{fontSize: 15}}>Sub Total</Text>
-                    <Text style={{fontSize: 15}}>
+                    <Text style={{ fontSize: 15 }}>Sub Total</Text>
+                    <Text style={{ fontSize: 15 }}>
                       {' '}
                       <PriceTag price={9500.0} clear />
                     </Text>
@@ -487,10 +491,10 @@ export const Cart = () => {
                       flexDirection: 'row',
                       justifyContent: 'space-between',
                     }}>
-                    <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
                       Total
                     </Text>
-                    <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
                       <PriceTag price={10000.0} clear />
                     </Text>
                   </View>
